15d3a0590328c8bfd3cc4dd1e8356d09f1dd3ad2
kez103 <kez103@mail.ru>
2018-01-04 15:38:21 +0400

    Subproc

--- recogn.py
+++ recogn.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
-import os
-# import subprocess as sb
+# import os
+import subprocess as sb
 
 r = sr.Recognizer()
 # r.energy_threshold = 1500000
@@ -19,12 +19,14 @@ with sr.Microphone() as source:
         try:
             com = r.recognize_google(audio, language="ru-RU")
             if "узык" in com:
-                # sb.popen(["D:\Soft\AIMP3\AIMP3.exe"])
-                os.popen("D:\Soft\AIMP3\AIMP3.exe /play")
+                # sb.Popen("aimp3 /play", shell=True)
+                sb.Popen("D:\Soft\AIMP3\AIMP3.exe /play", shell=True)
+                # os.popen("D:\Soft\AIMP3\AIMP3.exe /play")
                 print(com)
                 # break
             elif "топ" in com:
-                os.popen("D:\Soft\AIMP3\AIMP3.exe /pause")
+                # os.popen("D:\Soft\AIMP3\AIMP3.exe /pause")
+                sb.Popen("D:\Soft\AIMP3\AIMP3.exe /pause", shell=True)
                 print(com)
             else:
                 print(com)
