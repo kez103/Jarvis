245908de4d987808f09ddd55b14e3c53c6093026
kez103 <kez103@mail.ru>
2018-01-04 15:14:41 +0400

    Music, stop

--- recogn.py
+++ recogn.py
@@ -1,19 +1,36 @@
 import speech_recognition as sr
+import os
+# import subprocess as sb
 
 r = sr.Recognizer()
-r.energy_threshold = 500
+# r.energy_threshold = 1500000
+r.dynamic_energy_threshold = True  # type: bool
+r.dynamic_energy_adjustment_ratio = 1.1  # type: float
+
 
 with sr.Microphone() as source:
 
     while True:
 
+        # r.adjust_for_ambient_noise(source, 2)
         print("Скажите что-нибудь")
         audio = r.listen(source)
 
         try:
-            print(r.recognize_google(audio, language="ru-RU"))
+            com = r.recognize_google(audio, language="ru-RU")
+            if "узык" in com:
+                # sb.popen(["D:\Soft\AIMP3\AIMP3.exe"])
+                os.popen("D:\Soft\AIMP3\AIMP3.exe /play")
+                print(com)
+                # break
+            elif "топ" in com:
+                os.popen("D:\Soft\AIMP3\AIMP3.exe /pause")
+                print(com)
+            else:
+                print(com)
+
         except sr.UnknownValueError:
             print("Робот не расслышал фразу")
-            # pass
+
         except sr.RequestError as e:
             print("Ошибка сервиса; {0}".format(e))
